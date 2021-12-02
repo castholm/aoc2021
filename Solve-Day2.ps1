a42d64e3d1f75eb332b751e088ca966a915ec76f
Carl Åstholm <carl@astholm.se>
2021-12-02 10:12:32 +0100

    Day 2

---
+++ Solve-Day2.ps1
@@ -0,0 +1,67 @@
+﻿function Solve-Day2Part1 {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [string]
+        $CurrentCommand
+    )
+    begin {
+        $Position = 0
+        $Depth = 0
+    }
+    process {
+        $Instruction, $Argument = Parse-Command $CurrentCommand
+        switch ($Instruction) {
+        "forward" {
+            $Position += $Argument
+        }
+        "down" {
+            $Depth += $Argument
+        }
+        "up" {
+            $Depth -= $Argument
+        }
+        }
+    }
+    end {
+        $Position * $Depth
+    }
+}
+
+function Solve-Day2Part2 {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [string]
+        $CurrentCommand
+    )
+    begin {
+        $Aim = 0
+        $Position = 0
+        $Depth = 0
+    }
+    process {
+        $Instruction, $Argument = Parse-Command $CurrentCommand
+        switch ($Instruction) {
+        "down" {
+            $Aim += $Argument
+        }
+        "up" {
+            $Aim -= $Argument
+        }
+        "forward" {
+            $Position += $Argument
+            $Depth += $Aim * $Argument
+        }
+        }
+    }
+    end {
+        $Position * $Depth
+    }
+}
+
+function Parse-Command($Command) {
+    $null = $CurrentCommand -cmatch '^(forward|down|up) ([0-9])$'
+
+    $Matches[1], $Matches[2]
+}
